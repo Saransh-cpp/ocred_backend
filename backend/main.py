a7a0303af2a848d0b17452e2675bd84576df02a5
Saransh <saransh0701@gmail.com>
2021-11-23 02:06:14 +0530

    Add aiview_ocr

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from aiview_ocr import OCR
 
 app = FastAPI()
 
-@app.get('/')
+
+class Image(BaseModel):
+    image: str
+
+
+@app.get("/")
 def home():
     return {"message": "Hello World!"}
 
-@app.get('/ocr')
-def ocr():
-    return {"message": "OCR"}
+
+@app.post("/ocr-meaningful-text")
+async def ocr(image: Image):
+    ocr = OCR(
+        True,
+        "/image.jpg",
+        r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe"
+    )
+    text = ocr.ocr_meaningful_text()
+    return {"extracted text": text}
