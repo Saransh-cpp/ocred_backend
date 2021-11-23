6ed50e00f042aee9f8410acfcb79035b0c9da31b
Saransh <saransh0701@gmail.com>
2021-11-23 14:47:49 +0530

    OCR working

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,25 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+import os
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
 from aiview_ocr import OCR
+
 
 app = FastAPI()
 
 
-class Image(BaseModel):
-    image: str
-
-
 @app.get("/")
 def home():
-    return {"message": "Hello World!"}
+    return {"message": "AiView backend. Use the endpoint 'docs' for more information."}
 
 
 @app.post("/ocr-meaningful-text")
-async def ocr(image: Image):
-    ocr = OCR(
-        True,
-        "/image.jpg",
-        r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe"
-    )
+async def ocr_endpoint(image: UploadFile = File(...)):
+
+    with open("image.jpg", "wb+") as f:
+        f.write(image.file.read())
+        f.close()
+
+    ocr = OCR(False, "image.jpg", r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe")
     text = ocr.ocr_meaningful_text()
-    return {"extracted text": text}
+
+    return {"extracted text": text, "OCRed file": FileResponse("OCR.png")}
