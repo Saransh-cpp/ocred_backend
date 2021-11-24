d4b5e46130ade45e62b608365471d67c217d0d96
Saransh <saransh0701@gmail.com>
2021-11-24 14:50:20 +0530

    Backend working for OCR

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-import os
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from typing import List
 from aiview_ocr import OCR
+from fastapi.params import Query
+from fastapi.responses import FileResponse
+from fastapi import FastAPI, File, UploadFile
 
 
 app = FastAPI()
@@ -13,13 +14,51 @@ def home():
 
 
 @app.post("/ocr-meaningful-text")
-async def ocr_endpoint(image: UploadFile = File(...)):
+async def ocr_meaningful_text_endpoint(image: UploadFile = File(...)):
 
     with open("image.jpg", "wb+") as f:
         f.write(image.file.read())
-        f.close()
 
+    # replace the tesseract location with the location in your environment
+    # TODO: change the location here when deploying
     ocr = OCR(False, "image.jpg", r"D:\Saransh\Softwares\Tesseract-OCR\tesseract.exe")
     text = ocr.ocr_meaningful_text()
 
     return {"extracted text": text, "OCRed file": FileResponse("OCR.png")}
+
+
+@app.post("/ocr-bill")
+async def ocr_bill_endpoint(
+    languages: List[str] = Query(["en"]), image: UploadFile = File(...)
+):
+
+    with open("image.jpg", "wb+") as f:
+        f.write(image.file.read())
+
+    ocr = OCR(False, "image.jpg")
+    text = ocr.ocr_sparse_text(languages=languages)
+
+    info = ocr.process_extracted_text_from_invoice()
+
+    return {
+        "extracted text": text,
+        "extracted information": info,
+        "OCRed file": FileResponse("OCR.png"),
+    }
+
+
+@app.post("/ocr-sign-board")
+async def ocr_sign_board_endpoint(
+    languages: List[str] = Query(["en", "hi"]), image: UploadFile = File(...)
+):
+
+    with open("image.jpg", "wb+") as f:
+        f.write(image.file.read())
+
+    ocr = OCR(False, "image.jpg")
+    text = ocr.ocr_sparse_text(languages=languages)
+
+    return {
+        "extracted text": text,
+        "OCRed file": FileResponse("OCR.png"),
+    }
